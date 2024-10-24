a5550fabcfd2053862c635bfefade8d55fd0afb1
HienDuong <hien.duong@asnet.com.vn>
2024-10-24 17:06:12 +0700

    Chapter 3 Optimizing Fonts and Image

--- examples/nextjs-tutorial-learning/nextjs-dashboard/app/layout.tsx
+++ examples/nextjs-tutorial-learning/nextjs-dashboard/app/layout.tsx
@@ -1,4 +1,8 @@
+// Styles
 import "@/app/ui/global.css";
+
+// Fonts
+import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
   children,
@@ -7,7 +11,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      // Add the font by className
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
