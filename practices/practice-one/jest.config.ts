49f2f13c53eed69d4f2740cd8f730a74fdb6e895
Hien The Duong <hien.duong@asnet.com.vn>
2024-11-15 17:08:31 +0700

    Implement icon component

--- practices/practice-one/jest.config.ts
+++ practices/practice-one/jest.config.ts
@@ -14,9 +14,17 @@ const config: Config = {
     '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!<rootDir>/src/**/*.stories.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/constants/*',
+    '!<rootDir>/src/enums/*',
     '!<rootDir>/node_modules/',
   ],
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/index\\.ts$',
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
