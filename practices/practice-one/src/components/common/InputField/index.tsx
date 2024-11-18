ff3bc80a303b4fc0902cfe6710085c244264b960
Hien The Duong <hien.duong@asnet.com.vn>
2024-11-18 16:36:52 +0700

    Implement inpufiled component

---
+++ practices/practice-one/src/components/common/InputField/index.tsx
@@ -0,0 +1,67 @@
+// Libraries
+import { memo, forwardRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+import classNames from 'classnames';
+
+// Types
+import type { InputHTMLAttributes, ReactNode } from 'react';
+
+export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  customClasses?: string;
+  errorMessage?: string;
+  startContent?: ReactNode;
+  endContent?: ReactNode;
+}
+
+const baseClass: string = 'flex-1 outline-none px-3 py-2 bg-transparent';
+const containerBaseClass: string =
+  'flex items-center gap-1 px-3 border rounded-md focus-within:ring-2';
+const errorContainerClass: string =
+  'border-danger-200 focus-within:ring-danger-200';
+const normalContainerClass: string =
+  'border-gray-300 focus-within:ring-primary-100';
+const errorMessagesClasses: string = 'text-sm text-danger-200';
+
+/**
+ * InputField uncontrolled component
+ *
+ * @returns {JSX.Element} - InputField element.
+ */
+const InputField = forwardRef<HTMLInputElement, IInputProps>(
+  (
+    {
+      customClasses = '',
+      errorMessage = '',
+      startContent = null,
+      endContent = null,
+      ...restProps
+    },
+    ref,
+  ): JSX.Element => {
+    const containerClasses = twMerge(
+      classNames(
+        containerBaseClass,
+        errorMessage ? errorContainerClass : normalContainerClass,
+      ),
+    );
+
+    const combinedClasses = twMerge(baseClass, customClasses);
+
+    return (
+      <div>
+        <div className={containerClasses}>
+          {startContent && <span>{startContent}</span>}
+          <input ref={ref} className={combinedClasses} {...restProps} />
+          {endContent && <span>{endContent}</span>}
+        </div>
+        {errorMessage && (
+          <span className={errorMessagesClasses}>{errorMessage}</span>
+        )}
+      </div>
+    );
+  },
+);
+
+InputField.displayName = 'InputField';
+
+export default memo(InputField);
