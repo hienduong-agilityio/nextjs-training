b15b5b13716cdb47ea38abf36df419e11770fec8
HienDuong <hien.duong@asnet.com.vn>
2024-11-19 17:26:09 +0700

    #11 feat: implement ui for tabs components

--- practices/practice-one/src/app/layout.tsx
+++ practices/practice-one/src/app/layout.tsx
@@ -9,6 +9,7 @@ import './globals.css';
 
 // Components
 import { Header } from '@/components';
+import ProductTabs from '@/components/Tabs';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         <main className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12">
           <Header />
+          <ProductTabs />
           {children}
         </main>
       </body>
