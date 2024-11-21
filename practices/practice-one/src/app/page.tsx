327251f33e9efd6ecc8802c49f3adb967d7ec091
HienDuong <hien.duong@asnet.com.vn>
2024-11-21 13:54:12 +0700

    fix: remove useless code

--- practices/practice-one/src/app/page.tsx
+++ practices/practice-one/src/app/page.tsx
@@ -1,7 +1,5 @@
-import { ProductTabs } from '@/components';
-
 const Homepage = () => {
-  return <ProductTabs />;
+  return <h1 className="text-lg font-semibold">This is homepage</h1>;
 };
 
 export default Homepage;
--- practices/practice-one/src/components/index.ts
+++ practices/practice-one/src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './Header';
 export { default as SearchBox } from './SearchBox';
 export * from './SearchBox';
-export * from './ProductTab';
 
 // Common
 export * from './common';
