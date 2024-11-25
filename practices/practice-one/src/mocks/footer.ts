1c38ce7d6d12bae2f1c5448c1ea8871423477746
HienDuong <hien.duong@asnet.com.vn>
2024-11-25 14:52:25 +0700

    #16 feat: implement footer component

---
+++ practices/practice-one/src/mocks/footer.ts
@@ -0,0 +1,38 @@
+export const FOOTER_NAVIGATION = [
+  {
+    title: 'Information',
+    items: [
+      { name: 'About Us', url: '/about-us' },
+      { name: 'Information', url: '/information' },
+      { name: 'Privacy Policy', url: '/privacy-policy' },
+      { name: 'Terms & Conditions', url: '/terms-and-conditions' },
+    ],
+  },
+  {
+    title: 'Service',
+    items: [
+      { name: 'About Us', url: '/about-us' },
+      { name: 'Information', url: '/information' },
+      { name: 'Privacy Policy', url: '/privacy-policy' },
+      { name: 'Terms & Conditions', url: '/terms-and-conditions' },
+    ],
+  },
+  {
+    title: 'My Account',
+    items: [
+      { name: 'About Us', url: '/about-us' },
+      { name: 'Information', url: '/information' },
+      { name: 'Privacy Policy', url: '/privacy-policy' },
+      { name: 'Terms & Conditions', url: '/terms-and-conditions' },
+    ],
+  },
+  {
+    title: 'Our Offers',
+    items: [
+      { name: 'About Us', url: '/about-us' },
+      { name: 'Information', url: '/information' },
+      { name: 'Privacy Policy', url: '/privacy-policy' },
+      { name: 'Terms & Conditions', url: '/terms-and-conditions' },
+    ],
+  },
+];
