70b5f07a4cd912a37f1f614b1afa2878567378e2
HienDuong <hien.duong@asnet.com.vn>
2024-11-27 13:21:06 +0700

    feat: implement select common component

--- practices/practice-one/src/components/common/index.ts
+++ practices/practice-one/src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as InputField } from './InputField';
 export { default as Tabs } from './Tabs';
+export { default as Select } from './Select';
