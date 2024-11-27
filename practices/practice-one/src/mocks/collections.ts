1f2905e1cd491baf145169c82eef68ee53f85388
HienDuong <hien.duong@asnet.com.vn>
2024-11-27 13:26:26 +0700

    #22 feat: implement UI for Collection page

---
+++ practices/practice-one/src/mocks/collections.ts
@@ -0,0 +1,18 @@
+export const HOT_DEALS = [
+  { name: 'Nike', count: 2 },
+  { name: 'Airmax', count: 48 },
+  { name: 'Adidas', count: 95 },
+  { name: 'Vans', count: 23 },
+  { name: 'All Stars', count: 1 },
+];
+
+export const BRANDS = [
+  { name: 'Nike', count: 99 },
+  { name: 'Adidas', count: 99 },
+  { name: 'Siemens', count: 99 },
+];
+
+export const FILTER_GROUP = [
+  { title: 'Hot Deals', items: HOT_DEALS },
+  { title: 'BRAND', items: BRANDS },
+];
