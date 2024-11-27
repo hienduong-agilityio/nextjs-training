8c17fb37e38efd113b5eb11050e6efec1c260df4
HienDuong <hien.duong@asnet.com.vn>
2024-11-27 08:14:55 +0700

    fix: update metadata for product page

--- practices/practice-one/src/app/products/page.tsx
+++ practices/practice-one/src/app/products/page.tsx
@@ -4,16 +4,6 @@ import { getProducts } from '@/services';
 // Components
 import { ProductTabs } from '@/components';
 
-// Types
-import type { Metadata } from 'next';
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: 'Products',
-    description: `The Products landing page`,
-  };
-}
-
 export default async function ProductPage() {
   const productData = await getProducts();
 
