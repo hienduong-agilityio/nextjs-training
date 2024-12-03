81770cbf508ef43c54625a06e366026c60110a2c
HienDuong <hien.duong@asnet.com.vn>
2024-12-03 15:44:17 +0700

    #29 fix: update test for tabs component

--- practices/practice-one/src/components/common/Tabs/__test__/Tabs.test.tsx
+++ practices/practice-one/src/components/common/Tabs/__test__/Tabs.test.tsx
@@ -3,9 +3,9 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { Tabs } from '@/components';
 
 const mockItems = [
-  { title: 'Tab 1', content: <div>Content for Tab 1</div> },
-  { title: 'Tab 2', content: <div>Content for Tab 2</div> },
-  { title: 'Tab 3', content: <div>Content for Tab 3</div> },
+  { title: 'Tab 1', href: '/tab1', content: <div>Content for Tab 1</div> },
+  { title: 'Tab 2', href: '/tab2', content: <div>Content for Tab 2</div> },
+  { title: 'Tab 3', href: '/tab3', content: <div>Content for Tab 3</div> },
 ];
 
 describe('Tabs Component', () => {
@@ -34,7 +34,7 @@ describe('Tabs Component', () => {
   });
 
   it('renders the correct number of tabs', () => {
-    const tabs = screen.getAllByRole('button');
+    const tabs = screen.getAllByRole('link');
     expect(tabs).toHaveLength(mockItems.length);
 
     tabs.forEach((tab, index) => {
@@ -48,21 +48,21 @@ describe('Tabs Component', () => {
   });
 
   it('applies correct classes for active tabs', () => {
-    const activeTab = screen.getByRole('button', { name: /Tab 1/i });
+    const activeTab = screen.getByRole('link', { name: /Tab 1/i });
     expect(activeTab).toHaveClass(
       'text-primary-400 border-primary-400 border-b-2',
     );
   });
 
   it('applies correct classes for inactive tabs', () => {
-    const inactiveTab = screen.getByRole('button', { name: /Tab 2/i });
+    const inactiveTab = screen.getByRole('link', { name: /Tab 2/i });
     expect(inactiveTab).toHaveClass(
       'p-3 text-sm font-medium outline-none border-b-0',
     );
   });
 
   it('calls onTabChange when a tab is clicked', () => {
-    const tab2 = screen.getByRole('button', { name: /Tab 2/i });
+    const tab2 = screen.getByRole('link', { name: /Tab 2/i });
     fireEvent.click(tab2);
 
     expect(mockOnTabChange).toHaveBeenCalledTimes(1);
@@ -70,7 +70,7 @@ describe('Tabs Component', () => {
   });
 
   it('updates content when a new tab is selected', () => {
-    const tab2 = screen.getByRole('button', { name: /Tab 2/i });
+    const tab2 = screen.getByRole('link', { name: /Tab 2/i });
     fireEvent.click(tab2);
 
     const newActiveContent = screen.getByText('Content for Tab 2');
