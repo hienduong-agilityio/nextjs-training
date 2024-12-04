aef814cfc90a03955de34beadecc668a8e8a29f8
HienDuong <hien.duong@asnet.com.vn>
2024-12-04 17:13:14 +0700

    fix: udpate header responsive component

--- practices/practice-one/src/components/Header/index.tsx
+++ practices/practice-one/src/components/Header/index.tsx
@@ -1,11 +1,5 @@
 // Icon
-import {
-  CartIcon,
-  LogoIcon,
-  ProfileIcon,
-  SearchIcon,
-  MenuGroupIcon,
-} from '@/icons';
+import { CartIcon, LogoIcon, ProfileIcon, SearchIcon } from '@/icons';
 
 // Components
 import { SearchBox } from '@/components';
@@ -16,7 +10,7 @@ interface IHeaderProps {
 
 export const Header = ({ cartItemCount = 0 }: IHeaderProps) => {
   return (
-    <header className="container flex gap-2 items-center justify-between w-full px-4 py-4 mx-auto sm:px-6 md:px-8 lg:px-12 xl:px-16">
+    <header className="container flex gap-6 items-center justify-between w-full px-4 md:mt-10 py-4 mx-auto sm:px-6 md:px-8 lg:px-12 xl:px-16">
       {/* Logo */}
       <a href="/" className="flex items-center gap-2">
         <LogoIcon aria-label="logo" />
@@ -26,36 +20,33 @@ export const Header = ({ cartItemCount = 0 }: IHeaderProps) => {
       </a>
 
       {/* Profile, Cart, or Menu */}
-      <div className="w-3/4 flex items-center justify-end md:justify-between gap-4">
+      <div className="w-full flex items-center justify-end lg:justify-between gap-4">
         {/* Search */}
-        <div className="hidden md:block">
+        <div className="hidden lg:block">
           <SearchBox
             customClass={{
               container: 'md:justify-end',
-              input: 'w-full lg:w-[220px] xl:w-[520px]',
+              input: 'w-full lg:w-[400px]',
               inputContainer: 'border-primary-300',
               button: 'bg-primary-300 text-white',
             }}
           />
         </div>
-        <div className="block md:hidden">
+        <div className="block lg:hidden">
           <SearchIcon size={24} />
         </div>
 
         <div className="flex items-center gap-2">
-          <div className="hidden md:flex items-center gap-2">
+          <div className="flex items-center gap-2">
             {/* Profile */}
             <ProfileIcon aria-label="profile" size={20} />
-            <span className="hidden text-sm md:text-base lg:text-lg md:block">
+            <span className="hidden text-sm w-max md:text-base lg:text-lg lg:block">
               My account
             </span>
             <div className="relative flex items-center">
               {/* Cart */}
               <CartIcon aria-label="cart" size={25} itemCount={cartItemCount} />
             </div>
-          </div>
-          <div className="md:hidden">
-            <MenuGroupIcon size={24} />
           </div>
         </div>
       </div>
