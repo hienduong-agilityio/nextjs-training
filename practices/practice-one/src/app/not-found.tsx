faf274ef4f60f9692594d41d271b1d111d834a03
HienDuong <hien.duong@asnet.com.vn>
2024-12-18 13:58:34 +0700

    fix: update not found page

--- practices/practice-one/src/app/not-found.tsx
+++ practices/practice-one/src/app/not-found.tsx
@@ -5,15 +5,14 @@ import { ROUTE } from '@/constants';
 
 export default function NotFound() {
   return (
-    <section className="flex flex-col gap-10 items-center justify-center h-svh text-center px-4">
-      <h1 className="text-8xl font-bold text-gray-800">404 Not Found</h1>
-      <p className="text-lg text-gray-600 mt-4">
-        Sorry, the page you’re looking for doesn’t exist. You can go back to the
-        homepage.
+    <section className="flex flex-col gap-10 items-center justify-center h-[50vh] text-center px-4">
+      <h1 className="text-8xl font-medium text-gray-800">404 Not Found</h1>
+      <p className="text-base leading-6 font-normal text-gray-600 mt-4">
+        Your visited page not found. You may go home page.
       </p>
       <Link
         href={ROUTE.PRODUCTS}
-        className="mt-6 px-6 py-3 bg-blue-600 text-white text-lg font-medium rounded-md hover:bg-blue-700 transition"
+        className="mt-6 w-1/4 px-6 py-4 bg-primary-100 text-white text-lg font-medium rounded-md hover:bg-primary-200 transition"
       >
         Back to Home Page
       </Link>
